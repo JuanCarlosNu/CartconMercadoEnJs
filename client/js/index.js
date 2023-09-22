7fdad09c7aa2af97eccc070a7e8231fe76a9d681
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-22 17:48:11 -0300

    created all products cards

---
+++ client/js/index.js
@@ -0,0 +1,19 @@
+const shopContent = document.getElementById("shopContent");
+
+productos.forEach((product)=>{
+    const content = document.createElement("div");
+    content.innerHTML = `<img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>${product.price} $ </p>
+    `;
+    shopContent.append(content);
+    
+    const buyButton = document.createElement("button");
+    
+    buyButton.innerText= "Buy";
+    
+    content.append(buyButton);
+    
+
+    
+});
---
+++ client/js/products.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        id: 1,
+        productName: "naranja",
+        price: 10,
+        quantity: 1,
+        img: "./media/naranja.jpeg",
+    },
+    {
+        id: 2,
+        productName: "banana",
+        price: 15,
+        quantity: 1,
+        img: "./media/banana.png",
+    },
+    {
+        id: 3,
+        productName: "tomate",
+        price: 8,
+        quantity: 1,
+        img: "./media/tomate.png",
+    },
+
+];
