b0ec56f44448572409feb663618e2c1f9630283f
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-26 10:39:36 -0300

    created modal header with close button.

--- client/js/cart.js
+++ client/js/cart.js
@@ -4,7 +4,18 @@ const modalOverlay = document.getElementById("modal-overlay");
 
 const displayCart = ()=>{
     //modal Header
-     
-    const modalHeader = document.createElement("div"); 
+     const modalHeader = document.createElement("div"); 
+    const modalClose = document.createElement("span");
+    modalClose.innerText= "❌";
+    modalClose.className= "modal-close";
+    modalHeader.append(modalClose);
+
+    const modalTitle= document.createElement("div");
+    modalTitle.innerText= "carrio";
+    modalTitle.className= "modal-title";
+    modalHeader.append(modalTitle);
+
+    modalContainer.append(modalHeader); 
+
 
 }
